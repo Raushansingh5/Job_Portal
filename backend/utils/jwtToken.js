8086a1d207104a0657dbf87b5b680403b8d044cc
Raushansingh5 <rajatraushan5@gmail.com>
2025-08-30 12:42:43 +0530

    minor changes in cookie

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -1,17 +1,21 @@
 export const sendToken = (user, statusCode, res, message) => {
   const token = user.getJWTToken();
+
   const options = {
     expires: new Date(
-      Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+      Date.now() + Number(process.env.COOKIE_EXPIRE) * 24 * 60 * 60 * 1000
     ),
-    httpOnly: true, 
-    sameSite: "None",
+    httpOnly: true,   // prevents JS access
+    secure: true,     // important for SameSite=None
+    sameSite: "None", // allows cross-site cookies
   };
 
-  res.status(statusCode).cookie("token", token, options).json({
-    success: true,
-    user,
-    message,
-    token,
-  });
+  res
+    .status(statusCode)
+    .cookie("token", token, options)
+    .json({
+      success: true,
+      user,
+      message,
+    });
 };
